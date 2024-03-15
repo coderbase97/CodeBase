77a97cfd133d8a0c805be333240eba198d07e378
coderbase97 <163534188+coderbase97@users.noreply.github.com>
2024-03-15 13:33:34 +0000

    01_emptyvariable

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -3,10 +3,12 @@ let accountEmail ="coder.base.97@gmail.com"
 var accountPassword = 'testpass'
 accountCity = 'any'
 
+let accountState;
+
 console.log(accountId);
 
 accountEmail = 'test@test'
 accountPassword = 'heysiri'
 accountCity = 'Jaipur'
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
