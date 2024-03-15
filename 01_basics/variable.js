b993e7721ec5d11a0d496eb7aab6b02b79e96c6e
coderbase97 <163534188+coderbase97@users.noreply.github.com>
2024-03-15 13:30:31 +0000

    01_variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 2343;
+let accountEmail ="coder.base.97@gmail.com"
+var accountPassword = 'testpass'
+accountCity = 'any'
+
+console.log(accountId);
+
+accountEmail = 'test@test'
+accountPassword = 'heysiri'
+accountCity = 'Jaipur'
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
