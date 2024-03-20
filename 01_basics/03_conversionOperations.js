83dd834774de24f5be53d91b6771d60425b98669
coderbase97 <163534188+coderbase97@users.noreply.github.com>
2024-03-20 05:48:19 +0000

    prefix and comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,8 +1,26 @@
 
 let score = 'gola';
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
+
+// console.log(+true);
+
+// console.log(+"");
+
+var a = 4;
+
+var b = a++;
+
+
+a = 5;
+
+b = ++ a;
+
+
+console.log(a);
+
+console.log(b);
