bcd1cbe9d5138666ed130797d6c914a3a0ff1ff0
coderbase97 <163534188+coderbase97@users.noreply.github.com>
2024-03-20 06:40:16 +0000

    Datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+//7 : string, bollen, number, null , undefined, symbol, bitint
+
+//Refrence (Non Primitive)
+
+// Array, Object, Function
+
+//symbol always provide unique value
+
+// const id = Symbol('234');
+
+// const anotherid = Symbol('234');
+
+// console.log(id == anotherid);
+
+// const bigNumber = 23456787654575n;
+
+// console.log(typeof bigNumber);
+
+
+//Arrays
+
+// const hero = ['Hulk' , 'CA' , 'Loki' , 'DS']
+
+// console.table(hero);
+
+// Objects
+
+let myobj = {
+    name:'Hulk',
+    color:'Green',
+    power:'Anger'
+}
+
+console.table(myobj);
